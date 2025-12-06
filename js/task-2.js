d963b7cf8c88171b286fdd5a639d2306b279a9ca
Dictator <dictatorbogdan@gmail.com>
2025-12-06 15:01:45 +0200

    add js-hw3 task-2

---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+const makeArray = function (firstArray, secondArray, maxLength) {
+  const res = [];
+  const arr = firstArray.concat(secondArray);
+  for (let i = 0; i < maxLength; i += 1) {
+    res.push(arr[i]);
+  }
+
+  return res;
+};
+
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
